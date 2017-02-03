aa987deb311eaea4baad1dc9318741cb5f9211c2
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-03 09:37:24 -0800

    [+] basic project structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralRoguelike
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager _instance = null;
+        public static GameManager Instance
+        {
+            get { return _instance; }
+            private set { _instance = value; }
+        }
+
+        private void Awake()
+        {
+            // enforce singleton pattern
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralRoguelike
+{
+    public class Loader : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameManager;
+
+        private void Awake()
+        {
+            if (GameManager.Instance == null)
+            {
+                Instantiate(gameManager);
+            }
+        }
+    }
+}
