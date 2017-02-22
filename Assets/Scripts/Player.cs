2994174961b784553a138ed5fb96f0756f2afd43
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-22 11:23:36 -0800

    [+] not-working input-detection ;)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,59 @@ namespace ProceduralRoguelike
 {
 	public class Player : MovingObject
 	{
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            var horizontal = 0;
+            var vertical = 0;
+
+            #if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+                horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+                vertical   = (int)(Input.GetAxisRaw("Vertical"));
+
+                // Limit movement to one axis per move.
+                if (horizontal != 0) { vertical = 0; }
+
+            #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+			
+			    if (Input.touchCount > 0)
+			    {
+				    var touch = Input.touches[0];
+                    var touchOrigin = touch.position;
+				    if (touch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+				    {
+					    var touchEnd = touch.position;
+					    var dx = touchEnd.x - touchOrigin.x;
+					    var dy = touchEnd.y - touchOrigin.y;
+
+                        // Prevent this touch event from being re-used.
+					    touchOrigin.x = -1;
+					
+                        // Limit movement to one axis per move by favoring the dominant axis.
+                        if (Mathf.Abs(dx) > Mathf.Abs(dy))
+                        {
+                            horizontal = dx > 0 ? 1 : -1;
+                        }
+                        else
+                        {
+                            vertical = dy > 0 ? 1 : -1;
+                        }
+				    }
+			    }
+			
+            #endif
+
+            if (horizontal != 0 || vertical != 0)
+            {
+                AttemptMove(horizontal, vertical);
+            }
+        }
+
         /// <summary>
         /// Action the Player takes when movement is blocked. For example, opening a chest, entering
         /// a dungeon via an exit tile, etc.
