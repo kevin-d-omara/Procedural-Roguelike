46f39ae001af5d23ab1c8aa6c1a1a6f971b70f2c
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-22 07:24:12 -0800

    [++] file heirarchy

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace ProceduralRoguelike
+{
+    [RequireComponent (typeof (SpriteRenderer))]
+    public class Floor : MonoBehaviour
+    {
+        [SerializeField] private FloorTiles floorTiles;
+
+        private void Start()
+        {
+            // randomly select a floor sprite from FloorTiles data
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            Sprite[] sprites = floorTiles.Sprites();
+            spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/FloorTiles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralRoguelike
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "Tiles/Floor", order = 1)]
+    public class FloorTiles : ScriptableObject
+    {
+        [SerializeField] private Sprite[] sprites;
+
+        public Sprite[] Sprites()
+        {
+            return sprites;
+        }
+    }
+}
