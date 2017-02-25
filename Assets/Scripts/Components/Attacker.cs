5ad3d3d62d50c535bd5b41f6f988cacf4a4d5062
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-25 14:38:34 -0800

    [+] starting framework for component system; [+] Zombie frame

---
+++ Assets/Scripts/Components/Attacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralRoguelike
+{
+	public class Attacker : MonoBehaviour
+	{
+        // Time that must be waited between making attacks.
+        [Range(0f, 2f)]
+        public float speed;
+
+        // Time after beginning an attack before the attack happens. (i.e. the wind-up)
+        [Range(0f, 0.5f)]
+        public float delay;
+
+        // How much damage is dealt.
+        [Range(1, 5)]
+        public int damage;
+
+        // How far the linecast is sent to determine targets.
+        [Range(0.75f, 2f)]
+        public float range;
+	}
+}
