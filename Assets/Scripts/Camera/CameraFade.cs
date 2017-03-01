fc6db5eed3d52050d1cb7243ee183f4b32f3f113
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-01 09:24:26 -0800

    [+] iTween plugin & camera fade-out

--- Assets/Scripts/Camera/CameraFade.cs
+++ Assets/Scripts/Camera/CameraFade.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 namespace ProceduralRoguelike
 {
+    [RequireComponent(typeof(Camera))]
 	public class CameraFade : MonoBehaviour
 	{
-		
-	}
+        [Range(0f, 10f)]
+        [SerializeField] private float fadeTime = 2f;
+
+        private void Awake()
+        {
+            iTween.CameraFadeAdd();
+        }
+
+        private void OnEnable()
+        {
+            PassageController.OnEnterDungeon += FadeOut;
+            PassageController.OnExitDungeon += FadeOut;
+        }
+
+        private void OnDisable()
+        {
+            PassageController.OnEnterDungeon -= FadeOut;
+            PassageController.OnExitDungeon -= FadeOut;
+        }
+
+        private void FadeOut(Vector2 _)
+        {
+            iTween.CameraFadeTo(1.0f, fadeTime);
+        }
+
+        private void FadeIn(Vector2 _)
+        {
+            iTween.CameraFadeTo(0.0f, fadeTime);
+        }
+    }
 }
