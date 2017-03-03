9a9d016ad21d02303475e33f809bab98c941f602
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-03 14:21:09 -0800

    [+] branches

--- Assets/Scripts/Testing/CreatePath.m
+++ Assets/Scripts/Testing/CreatePath.m
@@ -1,6 +1,12 @@
 function [path, inflectionPts] = CreatePath(origin, facing, depth, bearing, inflectionRate, stepSize)
 % Creates a path of constant bearing which is subject to random inflection.
-% Returns the path and inflection points.
+% Returns the path and inflection points (w/ angle).
+% Return:
+%   path    - 2xN vector of N pts on the path; [x, y]^-1
+%   inflection points - 4xM vector of M inflection pts on the path;
+%                       [x, y, theta, sign(dTheta)]
+%
+% Parameters:
 %   origin  - starting location; [x,y]
 %   facing  - starting angle (0° == East == [+1, 0])
 %   depth   - length of path; meters
@@ -8,14 +14,13 @@ function [path, inflectionPts] = CreatePath(origin, facing, depth, bearing, infl
 %   inflectionRate - how often inflection points occur (%/meter)
 %   stepSize - length of incremental steps (meter)
 
-        % Allocate storage
+        % Allocate storage.
         path = zeros(2, round(depth * 1/stepSize));
         path(:,1) = origin(:,1);
-        inflectionPts = zeros(2, round(depth * 1/stepSize));
+        inflectionPts = zeros(4, round(depth * 1/stepSize));
         inflectionIdx = 0;
         
-        
-        
+        % Calculate parameters.
         inflectionChance = inflectionRate * stepSize;
         dTheta = bearing * stepSize;
         theta = facing;
@@ -25,7 +30,9 @@ function [path, inflectionPts] = CreatePath(origin, facing, depth, bearing, infl
             if (rand < inflectionChance)
                 dTheta = -dTheta;
                 inflectionIdx = inflectionIdx + 1;
-                inflectionPts(:, inflectionIdx) = path(:,i-1);
+                inflectionPts(1:2, inflectionIdx) = path(:,i-1);
+                inflectionPts(3, inflectionIdx) = theta;
+                inflectionPts(4, inflectionIdx) = -sign(dTheta);
             end
 
             theta = theta + dTheta;
@@ -33,5 +40,6 @@ function [path, inflectionPts] = CreatePath(origin, facing, depth, bearing, infl
             path(2,i) = stepSize * sin(theta) + path(2,i-1);
         end
         
+        % Prune unused inflection points.
         inflectionPts = inflectionPts(:,1:inflectionIdx);
 end
--- Assets/Scripts/Testing/DungeonGenerator.m
+++ Assets/Scripts/Testing/DungeonGenerator.m
@@ -1,33 +1,61 @@
 clc;clear;clf;close all;
 
 figure('units','normalized','outerposition',[0 0 1 1])
-nPlotsX = 4;
-nPlotsY = 2;
+nPlotsX = 1;
+nPlotsY = 1;
 plotIdx = 0;
 for plotY=1:nPlotsY
     for plotX=1:nPlotsX
         % Starting parameters
         startFacing = deg2rad(RandomBetween(0,360));	% rad
+        origin = [0; 0];
 
-        % Path parameters
+        % Path parameters -------------------------------------------------
         depth = 50*plotY;           % meters
-        bearing = deg2rad(15);    % rad/meter; tangent to path
+        bearing = deg2rad(15);      % rad/meter; tangent to path
         inflectionRate = .4;        % percent/meter
         stepSize = .1;              % meters
         
-        [essentialPath, essentialInflectionPts] = CreatePath([0; 0], startFacing, depth, bearing, inflectionRate, stepSize);
+        [essentialPath, essentialInflectionPts] = CreatePath(origin, startFacing, depth, bearing, inflectionRate, stepSize);
 
-        % Branch parameters
-        numBranches = 2.5;          % branches/path;
-        deflection = deg2rad(RandomBetween(30,150));    % rad
+        % Branch parameters (essential path) ------------------------------
+        branchRate = 3;                 % branches/path;
+        branchDepth = depth/5;          % meters
+        branchBearing = deg2rad(35); % rad/meter; tangent to path
+        branchInflectionRate = .4;      % percent/meter
         
-        % Plot resulting path.
+        % Compute number of branches
+        numBranches = floor(branchRate);
+        if (rand < mod(branchRate,1))
+            numBranches = numBranches + 1;
+        end
+        
+        % Create branches
+        branches = zeros(2, round(branchDepth * 1/stepSize), numBranches);
+        branchInflectionPoints = cell(1, numBranches);
+        branchOriginPts = zeros(size(essentialInflectionPts, 1), numBranches);
+        for branchIdx=1:numBranches
+            % Choose random inflection point to branch from.
+            numEPts = size(essentialInflectionPts, 2);
+            branchOriginPts(:, branchIdx) = essentialInflectionPts(:,round(RandomBetween(1, numEPts)));
+            deflection = deg2rad(RandomBetween(15,100));    % rad
+            deflection = branchOriginPts(4, branchIdx) * deflection + branchOriginPts(3, branchIdx);
+            
+            % Create branch.
+            [branches(:,:,branchIdx), branchInflectionPoints{1, branchIdx}] = CreatePath(branchOriginPts(1:2, branchIdx), deflection, branchDepth, branchBearing, branchInflectionRate, stepSize);
+        end
+        
+        % Plot resulting path. --------------------------------------------
         plotIdx = plotIdx + 1;
         subplot(nPlotsY, nPlotsX, plotIdx);
         
         % essential path
-        plot(essentialPath(1,:), essentialPath(2,:), 'b', 'LineWidth', 2);
+        plot(essentialPath(1,:), essentialPath(2,:), '-r', 'LineWidth', 2);
         hold on
+        % branches
+        for branchIdx=1:numBranches
+           plot(branches(1,:,branchIdx), branches(2,:,branchIdx), '--b'); 
+        end
         % origin
         plot(essentialPath(1,1), essentialPath(2,1), 'or');
         % inflection points
