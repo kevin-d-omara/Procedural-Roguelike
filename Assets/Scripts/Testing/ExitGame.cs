5b6e86d03bd6b86d141a9b9d30c98c21181e4ac9
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-11-10 14:10:47 -0800

    Add "exit game" and make presentable
    Exit the game by pressing the escape key.

---
+++ Assets/Scripts/Testing/ExitGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TabletopCardCompanion
+{
+    public class ExitGame : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            }
+        }
+    }
+}
